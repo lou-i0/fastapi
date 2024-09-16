61cf0530cd042294676eb8e72759a22b92eaf375
lou-i0 <lothen123@googlemail.com>
2024-09-16 14:59:11 +0100

    update main_simple

--- main_simple.py
+++ main_simple.py
@@ -1,8 +1,5 @@
 #%% Import in the relevant libraries
 #===================================
-import pickle                       # Loading in objects like ML models etc
-
-
 from fastapi import FastAPI         # Create the API for our end user(s)
 from fastapi import HTTPException   # Set own HTTP error calls 
 
